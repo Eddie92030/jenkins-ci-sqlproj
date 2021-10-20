07294dfa942aa617f44a068ace6654aa816e1f51
Eddie Khor <eddie.khor@gmail.com>
2021-10-20 17:47:49 +0800

    change

--- EddieDB1/Tables/HerTable.sql
+++ EddieDB1/Tables/HerTable.sql
@@ -3,7 +3,11 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Product] NCHAR(10) NULL, 
     [Amount] NUMERIC NULL, 
-    [Price] NUMERIC NULL
+    [Price] NUMERIC NULL, 
+    [purchaseDate] DATETIME NULL
 )
 
 GO
+
+
+CREATE INDEX [IX_HerTable_Column] ON [dbo].[HerTable] (Product)
