3ae8216da4fa5d5553900f093a1b781eec51dbef
Eddie Khor <eddie.khor@gmail.com>
2021-10-25 16:54:44 +0800

    rm

--- EddieDB1/Tables/HerTable.sql
+++ EddieDB1/Tables/HerTable.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[HerTable]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [Product] NCHAR(100) NULL, 
+    [Product] NCHAR(10) NULL, 
     [Amount] NUMERIC NULL, 
     [Price] NUMERIC NULL, 
     [purchaseDate] DATETIME NULL, 
--- EddieDB1/Tables/Table1.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[Table1]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [mycol] NCHAR(10) NULL
-)
